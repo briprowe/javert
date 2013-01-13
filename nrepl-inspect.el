90714a881c91e9e066a662e7b7ddb2fe194332d5
Brian Rowe <briprowe@yahoo.com>
2013-01-12 23:51:54 -0500

    Add elisp to display the results of inspection of a value in a clojure buffer.

---
+++ nrepl-inspect.el
@@ -0,0 +1,41 @@
+(require 'cl)
+(require 'nrepl)
+
+(defun nrepl-inspect-clj-string (ns symbol)
+  (let ((header (format "\"Inspecting symbol: %s\"" symbol)))
+    (format "%s" `(do (require 'inspector.javert)
+                      (in-ns ,(format "'%s" ns))
+                      (inspector.javert/inspect ,(format "%s" symbol))))))
+
+(defun nrepl-inspect-print-list (value)
+  (cond ((eq (car value) :value) (nrepl-inspect-print-value (cadr value)))
+        ((eq (car value) :newline) (insert "\n"))))
+
+(defun nrepl-inspect-print-value (value)
+  (cond ((stringp value) (insert value))
+        ((symbolp value) (insert (format "%s" value)))
+        ((listp value) (nrepl-inspect-print-list value))))
+
+(defun nrepl-inspect-print-result (buffer result)
+  (with-current-buffer buffer
+    (let ((inhibit-read-only t))
+      (mapcar 'nrepl-inspect-print-value result))))
+
+(defun nrepl-inspect-clean-value (value)
+  "Make java types readable (both for humans and the elisp reader)."
+  (replace-regexp-in-string "#<[[:alpha:]]+\\([[:ascii:]]+?\\)>" "\"\\1\"" value))
+
+(defun nrepl-inspect ()
+  (interactive)
+  (let* ((code (nrepl-inspect-clj-string nrepl-buffer-ns (symbol-at-point)))
+         (inspect-buffer (nrepl-popup-buffer "*nREPL inspect*" t)))
+    (nrepl-send-string code (nrepl-make-response-handler
+                             inspect-buffer
+                             (lambda (buffer value)
+                               (nrepl-inspect-print-result buffer
+                                                           (read (nrepl-inspect-clean-value
+                                                                  (format "%s" value)))))
+                             '()
+                             (lambda (buffer str)
+                               (nrepl-emit-into-popup-buffer buffer "Error"))
+                             '()))))
